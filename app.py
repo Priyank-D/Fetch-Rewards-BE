d63cc12df4d31cce5a81eaff84b5e28c03d3ed3b
Priyank Kumar <pdharani@hawk.iit.edu>
2021-03-03 13:04:01 -0500

    Adding test unit and updating requirements.txt

--- app.py
+++ app.py
@@ -18,11 +18,13 @@ app=Flask(__name__)
 accounts = {}
 transactions_insert = []
 total_points = 0
+status_code = 200
 
 #1. Insert Transaction
 @app.route('/transactions', methods=['POST'])
 def transactions():
     global total_points
+    global status_code
     response = {}
     content = request.get_json()
     util.check_required_args(content,'payer','points','timestamp')
@@ -38,23 +40,28 @@ def transactions():
         transactions_insert.append(content)
         response["Suscess"] = "Record inserted successfully"
     elif points < 0 :
-        if payer in accounts and (accounts[payer] - points) < 0 :
+        if payer in accounts and (accounts[payer] + points) < 0 :
             response["Error"] = "Payer Amount is going negative"
-        elif payer in accounts and (accounts[payer] - points) > 0:
+            status_code = 400
+        elif payer in accounts and (accounts[payer] + points) > 0:
             total_points += points
             accounts[payer]+=points
             transactions_insert.append(content)
             response["Suscess"] = "Record inserted successfully"
         else:
             response["Error"] = "Not a Valid Transaction"
+            status_code = 400
+    else:
+        response["Warning"] = "Points value should be greater than 0"
     transactions_insert.sort(key= lambda x:x["timestamp"])
     
-    return response
+    return response,status_code
 	
 #2. Delete Points
 @app.route('/delete_points', methods=['POST'])
 def delete_points():
     global total_points
+    global status_code
     response = {}
     content = request.get_json()
     remaining_balance = {}
@@ -65,7 +72,8 @@ def delete_points():
     if total_points < poins_deduct:
         response["Error"] = "Not Enough Points Available"
         response["Available Points"] = total_points
-        return response
+        status_code = 400
+        return response,status_code
     else:
         for transid,transaction in enumerate(transactions_insert):
             chk_points = transaction["points"]
@@ -89,11 +97,12 @@ def delete_points():
     final_result = [
         {"payer":payer,"points":points} for payer,points in remaining_balance.items()
     ]
-    return json.dumps(final_result)
+    return json.dumps(final_result),status_code
 
 #3. Show transactions
 @app.route('/balance', methods=['GET'])
 def balance():
+	global status_code
 	balance = {}
 
 	for transaction in transactions_insert:
@@ -102,7 +111,7 @@ def balance():
 			balance[payer] = transaction["points"]
 		else:
 			balance[payer] += transaction["points"]
-	return balance
+	return balance,status_code
 
 if __name__ == '__name__':
     app.run(debug=True)
--- helpers/util.py
+++ helpers/util.py
@@ -6,5 +6,6 @@ Modefied by:
 
 def check_required_args(request,*args):
     for key in args:
-        if not request.get(key):
+        print(key)
+        if key not in request:
             raise KeyError('Arguments %s are required' % (', '.join(args)))
